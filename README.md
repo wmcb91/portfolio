cd36151d5c1673931e49d7b716cbab1df7643169
wmcb91 <wmcb91@gmail.com>
2016-11-22 13:22:00 -0500

    Edit styling and delete icons

---
+++ README.md
@@ -0,0 +1,7 @@
+# Will McBride
+
+Boston-based full stack web developer
+
+My Portfolio: [wmcbride.co](http://wmcbride.co)
+
+Resume: [pdf](https://github.com/wmcb91/myportfolio/master/McBride_Resume.pdf)
--- assets/js/devicon.js
+++ assets/js/devicon.js
@@ -1,13 +1,13 @@
-// var expand = function() {
-//   $('.dev').show();
-// };
-//
-// var shrink = function() {
-//   $('.dev').show();
-// };
-//
-// jQuery(document).ready(function () {
-//   $('.devicon').mouseenter(expand).mouseleave(shrink);
-//   // $('.dev').on('click', expand);
-//   // $('.dev js dev-expanded').hide();
-// });
+var expand = function() {
+  $('.dev').show();
+};
+
+var shrink = function() {
+  $('.dev').show();
+};
+
+jQuery(document).ready(function () {
+  $('.devicon').mouseenter(expand).mouseleave(shrink);
+  $('.dev').on('click', expand);
+  $('.dev js dev-expanded').hide();
+});
