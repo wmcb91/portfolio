da03387bee7aca495da9003e30b284ab3f8f6c9e
wmcb91 <wmcb91@gmail.com>
2017-02-09 18:04:04 -0500

    Portfolio version 2.0
    Changes:
    - Upload updated resume
    - Update skills section copy
    - Improve website responsiveness
      -- Line spacing, margins, borders and image sizes for project section
      in certain media sizes
      -- Include tip to click thumbnails for media sizes likely to be mobile
      devices
      -- Fix navbar issue for media sizes under 767

--- assets/js/nav-hide.js
+++ assets/js/nav-hide.js
@@ -1,20 +1,27 @@
 $(document).ready(function(){
 
-  if (document.body.clientWidth > 767) {
-    $(".navbar").hide();
-
+  if (document.body.clientWidth > 480) {
     $(function () {
       $(window).scroll(function () {
         if ($(this).scrollTop() > (window.innerHeight * 0.2)) {
-          $('.navbar').fadeIn();
+          $('.nav-reaction').fadeIn();
           $('.over-image-title').fadeOut(1050);
         } else {
-          $('.navbar').fadeOut();
+          $('.nav-reaction').fadeOut();
           $('.over-image-title').fadeIn(350);
         }
       });
     });
-  } else {
-    $(".navbar").show();
+  } if (document.body.clientWidth === 768 ||
+        document.body.clientWidth === 736 ||
+        document.body.clientWidth === 732 ||
+        document.body.clientWidth === 667 ||
+        document.body.clientWidth === 640 ||
+        document.body.clientWidth === 568
+        ) {
+    $('.tip-info').show();
+  } if (document.body.clientWidth <= 480) {
+    $('.nav-reaction').show();
+    $('.tip-info').show();
   }
 });
