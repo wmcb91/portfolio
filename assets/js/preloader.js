240e1992d2d7458727a562e80c8e4c42ceb58533
wmcb91 <wmcb91@gmail.com>
2017-08-22 09:31:21 -0400

    Notes for version 2.1.1 of portfolio
    - Add will-change property to snow before animation begins
    - Change -mini file endings to .min
    - Update resume file
    - Correct typo in original template to give line height to standart2title
    - Correct typo in original template changing 'standart' to 'standard'

--- assets/js/preloader.js
+++ assets/js/preloader.js
@@ -1,19 +1,20 @@
-function toggleFlurries(){
-	$(this).toggleClass('active');
-	$('.snow').toggleClass('flurries')
-}
-// Fade out snow effect and add toggle function to snowflake icon
-function endFlurries(){
-	$('.action-banner-bg').addClass('loaded');
-	setTimeout(function(){
-		$('#snow-toggle').fadeIn();
-		$('#snow-toggle').removeClass('active');
-		$('.snow').addClass('flurries');
-		$('#snow-toggle').on('click', toggleFlurries);
-	}, 5500);
-}
 
 $(document).ready(function(){
+	function toggleFlurries(){
+		$(this).toggleClass('active');
+		$('.snow').toggleClass('flurries')
+	}
+	// Fade out snow effect and add toggle function to snowflake icon
+	function endFlurries(){
+		$('.action-banner-bg').css('will-change', 'opacity').addClass('loaded');
+		setTimeout(function(){
+			$('#snow-toggle').fadeIn();
+			$('#snow-toggle').removeClass('active');
+			$('.snow').css('will-change', 'opacity').addClass('flurries');
+			$('#snow-toggle').on('click', toggleFlurries);
+		}, 5500);
+	}
+
 	endFlurries();
 	$('#nav').localScroll();
 	$('#nav').on('click', function() {
