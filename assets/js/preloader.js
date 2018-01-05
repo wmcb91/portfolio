2d4997e8bb62a6f4797ef9c3dfb954bbb132d9c8
wmcb91 <wmcb91@gmail.com>
2018-01-05 12:54:00 -0700

    Fix section scroll bug

--- assets/js/preloader.js
+++ assets/js/preloader.js
@@ -17,11 +17,12 @@ $(document).ready(function(){
 	endFlurries();
 	$('#nav a').localScroll();
 	$('#nav a').on('click', function() {
-		if ($(this).attr('href').includes('resume')) { return; }
+		if ($(this).attr('href').includes('resume')){return;}
+		event.preventDefault();
 		$('body').clearQueue();
 		$(event.target).blur();
 		var sectionTop = $(this.hash).position().top;
-		$('html, body').animate({scrollTop:sectionTop}, 500, 'linear');
+		$('html, body').animate({scrollTop:sectionTop}, 300, 'linear');
 	});
 
 	window.addEventListener("wheel", function(){$('body').stop();},{passive:true});
